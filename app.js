b16b565e798f26aaf825362da7fab3b3c8c83f5e
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2021-09-12 00:53:55 -0300

    Loading animation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 const NumberOfPokemon = 386 //Generation I, II and III (Red, Green, Blue, Yellow, Gold, Silver, Crystal, Ruby, Sapphire and Emerald)
+const spinnerLoading = document.querySelector("#loading")
 
 
 function capitalizeFirstLetter(string) {
@@ -11,6 +12,7 @@ const generatePokemonPromises = () => Array(NumberOfPokemon).fill().map((_, inde
     fetch(getPokemonUrl(index+1)).then(response => response.json()))
 
 
+    
 const generateHTML = pokemon => pokemon.reduce((accumulator, {name, id, types}) => {
     const elementTypes = types.map(typeInfo => capitalizeFirstLetter(typeInfo.type.name))
 
@@ -30,9 +32,14 @@ const insertPokemonIntoPage = pokemon => {
     ul.innerHTML = pokemon
 }
 
+const removeLoadingSpinner = () => {
+    spinnerLoading.remove()
+}
+
 
 const pokemonPromises = generatePokemonPromises()
 
 Promise.all(pokemonPromises)
     .then(generateHTML)
     .then(insertPokemonIntoPage)
+    .then(removeLoadingSpinner)
